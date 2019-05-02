ccdfa4db4b454045457d68da9dfacd7b941d705b
Damian Bielecki <bielik20@gmail.com>
2019-05-02 09:18:21 +0200

    Add separate imports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { DummyClass, first, second } from 'tree-shakeable-lib';
+import { DummyClass, first, second } from 'tree-shakeable-lib/dist/module/ad-engine';
+import { products } from 'tree-shakeable-lib/dist/module/ad-products';
 
 first();
+products();
