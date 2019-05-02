b90d1db8149294e62c40db2ae79ef7b0d199e7ca
Damian Bielecki <bielik20@gmail.com>
2019-05-02 10:23:01 +0200

    Change import path

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import { DummyClass, first, second } from 'tree-shakeable-lib/dist/module/ad-engine';
-import { products } from 'tree-shakeable-lib/dist/module/ad-products';
+import { DummyClass, first, second } from 'tree-shakeable-lib/modules/core';
+import { products, ProductsClass } from 'tree-shakeable-lib/modules/products';
 
 first();
 products();
+
+class DerivedClass extends ProductsClass {
+	print() {
+		// tslint:disable-next-line:no-console
+		console.log('from derived class ', this.surname);
+	}
+}
