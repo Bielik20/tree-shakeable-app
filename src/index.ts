18947bb9a206382990dfac15d21b7693a05b80b7
Damian Bielecki <bielik20@gmail.com>
2019-05-03 15:19:36 +0200

    Add ad-engine

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { utils } from '@wikia/ad-engine';
 import { DummyClass, first, second } from 'tree-shakeable-lib/modules/core';
 import { products, ProductsClass } from 'tree-shakeable-lib/modules/products';
 
@@ -8,5 +9,7 @@ class DerivedClass extends ProductsClass {
 	print() {
 		// tslint:disable-next-line:no-console
 		console.log('from derived class ', this.surname);
+
+		return new utils.LazyQueue();
 	}
 }
