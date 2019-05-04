bf8cf50965e9bdf27110222fdd9ef1aed442e0d5
Damian Bielecki <bielik20@gmail.com>
2019-05-04 13:04:56 +0200

    comment class

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import { first, ProductsClass } from 'tree-shakeable-lib';
 first();
 // products();
 //
-class DerivedClass extends ProductsClass {
-	print() {
-		// tslint:disable-next-line:no-console
-		console.log('from derived class ', this.surname);
-	}
-}
+// class DerivedClass extends ProductsClass {
+// 	print() {
+// 		// tslint:disable-next-line:no-console
+// 		console.log('from derived class ', this.surname);
+// 	}
+// }
