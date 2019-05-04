05de754cb407cf8623d9f9a6b18fc5e78341923c
Damian Bielecki <bielik20@gmail.com>
2019-05-04 10:25:35 +0200

    install babel

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { first } from 'tree-shakeable-lib';
+import { first, ProductsClass } from 'tree-shakeable-lib';
 
 first();
 // products();
 //
-// class DerivedClass extends ProductsClass {
-// 	print() {
-// 		// tslint:disable-next-line:no-console
-// 		console.log('from derived class ', this.surname);
-// 	}
-// }
+class DerivedClass extends ProductsClass {
+	print() {
+		// tslint:disable-next-line:no-console
+		console.log('from derived class ', this.surname);
+	}
+}
