74a934513e7e4098c473cc2017c300cca1f11997
Damian Bielecki <bielik20@gmail.com>
2019-05-04 10:00:32 +0200

    Add single import

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import { DummyClass, first, second } from 'tree-shakeable-lib/modules/core';
-import { products, ProductsClass } from 'tree-shakeable-lib/modules/products';
+import { first } from 'tree-shakeable-lib';
 
 first();
-products();
-
-class DerivedClass extends ProductsClass {
-	print() {
-		// tslint:disable-next-line:no-console
-		console.log('from derived class ', this.surname);
-	}
-}
+// products();
+//
+// class DerivedClass extends ProductsClass {
+// 	print() {
+// 		// tslint:disable-next-line:no-console
+// 		console.log('from derived class ', this.surname);
+// 	}
+// }
