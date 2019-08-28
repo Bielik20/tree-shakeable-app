1d034e99b3ba924d83c9f60997210ab1a1a737d4
Damian Bielecki <bielik20@gmail.com>
2019-08-28 09:48:34 +0200

    add broken scenario

--- src/core/dependency-class.ts
+++ src/core/dependency-class.ts
@@ -1,8 +1,20 @@
+import { Inject } from 'typescript-ioc';
+
+export class AppDependencyOfDependencyClass {
+	field: 'dep of dep';
+
+	constructor() {
+		console.log('constructor AppDependencyOfDependencyClass');
+	}
+}
+
 export class AppDependencyClass {
+	@Inject date111: Date;
 	name = 'bielik name 2';
 	surname = 'bielik surname 2';
 
-	constructor() {
+	constructor(@Inject private aaa: AppDependencyOfDependencyClass) {
 		console.log('constructor AppDependencyClass');
 	}
+	// constructor(@Inject date2: Date) {}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { LibConsumerClass, LibDependencyClass } from 'tree-shakeable-lib';
 import { Container, Scope } from 'typescript-ioc';
-import { AppDependencyClass } from './core';
+import { AppDependencyClass, AppDependencyOfDependencyClass } from './core';
 import { AppConsumerClass } from './products';
 
-// Container.bind(LibConsumerClass).to(AppConsumerClass);
-Container.bind(AppDependencyClass).scope(Scope.Local);
+Container.bind(LibConsumerClass).to(AppConsumerClass);
+// Container.bind(AppDependencyClass).scope(Scope.Local);
+Container.bind(AppDependencyOfDependencyClass);
 Container.bind(LibDependencyClass).to(AppDependencyClass);
 
 const instance: LibConsumerClass = Container.get(LibConsumerClass);
