c5cb0496ce4edb42a583337d747c1dc26f69bdcd
Damian Bielecki <bielik20@gmail.com>
2019-10-18 09:25:45 +0200

    add code from simulator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('father works');
+document.addEventListener('DOMContentLoaded', () => {
+	window.addEventListener(
+		'message',
+		(event) => {
+			console.log(event);
+			if (event.data === 'iframe message') {
+				event.source.postMessage('father message to iframe', '*' as any);
+			}
+		},
+		false,
+	);
+
+	const button = document.getElementById('post-message');
+
+	button.addEventListener('click', () => {
+		window.postMessage('father message', '*');
+	});
+});
