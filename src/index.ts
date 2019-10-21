afec9c649bf777371e137024b49a2c99540c57c6
Damian Bielecki <bielik20@gmail.com>
2019-10-21 11:46:52 +0200

    add closing

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
 	// tslint:disable-next-line:no-unused-expression
 	new Coordinator();
 	const communicator = new Communicator('ae', 'tree shakeable app');
-	const button = document.getElementById('post-message');
 
-	communicator.actions.subscribe((action) => console.log('tree shakeable app', action));
+	communicator.actions$.subscribe((action) => console.log('tree shakeable app', action));
 
-	button.addEventListener('click', () => {
+	document.getElementById('post-message').addEventListener('click', () => {
 		communicator.emit({ type: 'father message' });
+	});
+
+	document.getElementById('listen-actions').addEventListener('click', () => {
+		communicator.actions$.subscribe((action) => console.log('tree shakeable app (listen)', action));
+	});
+
+	document.getElementById('close-1').addEventListener('click', () => {
+		const iframe = document.getElementById('iframe-1');
+		iframe.parentNode.removeChild(iframe);
+	});
+
+	document.getElementById('close-2').addEventListener('click', () => {
+		const iframe = document.getElementById('iframe-2');
+		iframe.parentNode.removeChild(iframe);
 	});
 });
